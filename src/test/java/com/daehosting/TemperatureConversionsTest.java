6320d417f1e5ee2204059a47032328692bf8f435
ajpark30 <apark@madisoncollege.edu>
2019-04-05 19:30:55 -0500

    Adding the final three tests.

--- src/test/java/com/daehosting/TemperatureConversionsTest.java
+++ src/test/java/com/daehosting/TemperatureConversionsTest.java
@@ -10,10 +10,16 @@ class TemperatureConversionsTest {
 
     @Test
     void getTemperatureConversionsSoap() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.fahrenheitToCelsius(BigDecimal.valueOf(32));
+        assertEquals(BigDecimal.valueOf(0), result);
     }
 
     @Test
     void getTemperatureConversionsSoap1() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.windChillInFahrenheit(BigDecimal.valueOf(50), BigDecimal.valueOf(32));
+        assertEquals(BigDecimal.valueOf(18.75848), result);
     }
 
     @Test
@@ -25,5 +31,8 @@ class TemperatureConversionsTest {
 
     @Test
     void getTemperatureConversionsSoap121() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.windChillInCelsius(BigDecimal.valueOf(0), BigDecimal.valueOf(0));
+        assertEquals(BigDecimal.valueOf(1.41), result);
     }
 }
